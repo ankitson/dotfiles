1353b67d1c153d6cb53d238acbf343e9cb120a0b
Ankit Soni <ankitsoni9@gmail.com>
2021-03-17 07:34:45 -0700

    add docker compose alias

--- .alias.sh
+++ .alias.sh
@@ -6,4 +6,5 @@ alias g="git"
 #If the last character of the alias value is a blank, then the next command word following the alias is also checked for alias expansion.
 #https://www.gnu.org/software/bash/manual/bash.html#Aliases 
 #This allows sudo to work with aliases.
-alias sudo='sudo ' 
+alias sudo="sudo "
+alias dc="sudo docker-compose -f /home/ankit/homeserver/docker-compose.yaml"
