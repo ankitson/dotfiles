3794dc464856b0006b4a0ec8f763368fc7b50f55
Ankit Soni <dev@ankitson.com>
2026-01-30 19:47:12 -0800

    fix: delete leftover files

--- run_once_after_install-tmux-plugins.sh
+++ run_once_after_install-tmux-plugins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Install tmux plugins via tpm (after .chezmoiexternal has fetched tpm)
 if [ -x "$HOME/.tmux/plugins/tpm/bin/install_plugins" ]; then
-  "$HOME/.tmux/plugins/tpm/bin/install_plugins"
+  "$HOME/.tmux/plugins/tpm/bin/install_plugins" || true
 fi
